4e950b9bcb107dda571207d9adfbf25c5045309c
Saul Samuels Moselle <saulsamuelsmoselle@hotmail.com>
2023-03-23 09:18:57 +0100

    bad commit message

--- backend/util.py
+++ backend/util.py
@@ -1,5 +1,8 @@
-# Simulates a datebase connection 
+# Simulates a datebase connection, does some other util stuff 
+import csv
 import pandas as pd 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 class Connector(object):
     
@@ -23,9 +26,40 @@ class Connector(object):
 def print_transcript(convo):
     [print(f'{row["account_id"]}: {row["raw_message"]}') for id, row in convo.iterrows()]
         
+# import pandas as pd
+from multiprocessing import Pool
+# from nltk.sentiment import SentimentIntensityAnalyzer
 
+def add_sentiment_to_csv(path, outpath):
+    print("loading")
+    data = pd.read_csv(path)
+
+    print("loaded")
+    header = list(data.columns)
+    header.append("sentiment")
+
+    # create the SentimentIntensityAnalyzer object once
+    sid = SentimentIntensityAnalyzer()
+
+    # define a function to calculate the sentiment for a single row
+    def calculate_sentiment(row):
+        scores = sid.polarity_scores(row[4])
+        row.append(scores)
+        return row
+
+    # use a multiprocessing pool to calculate the sentiment for each row
+    with Pool() as pool:
+        results = pool.map(calculate_sentiment, data.itertuples(index=False))
+
+    # write the results to a new CSV file
+    with open(outpath, "w", encoding="utf-8") as of:
+        writer = csv.writer(of)
+        writer.writerow(header)
+        writer.writerows(results)
 if __name__ == "__main__":
-    paths = ["backend\data-store\chat_messages_1.csv\chat_messages_1.csv", "backend\data-store\chat_messages_2.csv\chat_messages_2.csv"]
+    # paths = ["backend\data-store\chat_messages_1.csv\chat_messages_1.csv", "backend\data-store\chat_messages_2.csv\chat_messages_2.csv"]
     # print(typeof)
-    print(con.get_conversation('11cf0f6b70280de8e21157a87da895c0db0dcc34222386600fb5d4959c88ee51'))
+    # print(con.get_conversation('11cf0f6b70280de8e21157a87da895c0db0dcc34222386600fb5d4959c88ee51'))
+    add_sentiment_to_csv('backend\data-store\chat_messages_1.csv\chat_messages_1.csv', 'backend\data-store\messages-1-with-sentiment.csv')
+
 
